cb3b36626f15f06c4b8f0cce0ff03d8b2a55d92a
ChenHaoWu1031 <123709902+ChenHaoWu1031@users.noreply.github.com>
2023-02-06 15:44:51 -0800

    Update main.cc

--- prob01/main.cc
+++ prob01/main.cc
@@ -1,7 +1,30 @@
 #include <iomanip>
 #include <iostream>
+using namespace std;
 
-int main() {
+int main() 
+{
   // TODO: Create a program that computes the restaurant bill.
+  double cost;
+  double tip;
+  double tip_percentage;
+  double tax;
+
+  cout<<"Input meal cost: ";
+  cin>>cost;
+  cout<<"Input tip percentage: ";
+  cin>> tip_percentage;
+
+  tax = 0.075 * cost;
+  tip = (tip_percentage/100)*cost;
+
+  cout<<"Restaurant Bill"<<endl;
+  cout<<"===================="<<endl;
+  cout<<"Subtotal: $"<<cost<<endl;
+  cout<<"Taxes: $"<<tax<<endl;
+  cout<<"Tip: $"<<tip<<endl;
+  cout<<"===================="<<endl;
+  cout<<"Total: $"<<cost+tax+tip;
+    
   return 0;
 }
